ee6ae59e4bac04e6e8810d820ec3488fffc75c64
Brian Lai <bjlai@ucsd.edu>
2014-03-03 18:54:23 -0800

    Changing project to sniffer

--- models.js
+++ models.js
@@ -3,10 +3,9 @@ var Mongoose = require('mongoose');
 
 
 var ProjectSchema = new Mongoose.Schema({
-  title: String,
-  date: {type: Date, default: Date.now},
-  summary: String,
-  image: String
+  mac: String,
+  pi: String,
+  time: String
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
